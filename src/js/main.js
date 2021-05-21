628ef9e104e16e88c326449d56df3ec4e05e48ee
Nikita Shvetsov <nshvtsv@gmail.com>
2021-05-21 06:18:31 -0700

    add leaders

--- src/js/main.js
+++ src/js/main.js
@@ -21,3 +21,27 @@ $(document).ready(function () {
     });
 
 });
+
+$(document).ready(function () {
+    $('.leaders-item-slider').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 5,
+        autoplay: false,
+        infinite: true,
+        speed: 1800,
+        adaptiveHeight: false,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        easing: 'ease',
+        draggale: true,
+        swipe: true,
+        touchTreshold: 20,
+        touchMove: false,
+        prevArrow: document.querySelector('#prevArrow-2'),
+        nextArrow: document.querySelector('#nextArrow-2')
+
+    });
+
+});

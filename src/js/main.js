bc15823e836b80886875cfa7f913896370eb7484
Nikita Shvetsov <nshvtsv@gmail.com>
2021-05-21 09:44:42 -0700

    add

--- src/js/main.js
+++ src/js/main.js
@@ -45,3 +45,27 @@ $(document).ready(function () {
     });
 
 });
+
+$(document).ready(function () {
+    $('.products-item-slider').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 5,
+        autoplay: false,
+        infinite: true,
+        speed: 1800,
+        adaptiveHeight: false,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        easing: 'ease',
+        draggale: true,
+        swipe: true,
+        touchTreshold: 20,
+        touchMove: false,
+        prevArrow: document.querySelector('#prevArrow-3'),
+        nextArrow: document.querySelector('#nextArrow-3')
+
+    });
+
+});
